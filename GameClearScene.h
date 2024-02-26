175dd722f1fb74c97a2cca8cf0e862e3628cb8bc
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2024-02-26 17:50:02 +0900

    クリアシーンとゲームオーバーの追加

---
+++ GameClearScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "BaseScene.h"
+
+class GameClearScene : public BaseScene {
+public:
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+	void Finalize();
+
+private:
+	
+
+};
---
+++ GameOverScene.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "BaseScene.h"
+
+class GameOverScene : public BaseScene {
+public:
+
+	void Initialize() override;
+
+	void Update() override;
+
+	void Draw() override;
+
+	void Finalize() override;
+
+private:
+	
+
+
+};
